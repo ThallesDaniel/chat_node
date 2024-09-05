62e49e1c8b2670876d0d34fb1c44a305b58f4f03
Thalles Daniel <106764514+ThallesDaniel@users.noreply.github.com>
2024-09-05 17:25:00 -0300

    servidor node simples

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,17 @@
-console.log('hello world!')
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+//verifica se possui a porta "PORT" nas variaveis de ambiente, caso contrário inicia na 8080
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
+
+wss.on("connection", (ws) => {
+    ws.on("error", console.error)
+
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log("Client successfully connected!!!")
+})
